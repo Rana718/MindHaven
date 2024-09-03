73b5ebf78744172cd14fffc7c28b861996c84332
Rana718 <ranadolui718@gmail.com>
2024-09-04 00:44:58 +0530

    improve styleing

--- app/(model)/adjust-timer.tsx
+++ app/(model)/adjust-timer.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
 import React, { useContext } from "react";
 import AppGradient from "@/components/AppGradient";
 import { router } from "expo-router";
@@ -15,25 +15,31 @@ const adjust_timer = () => {
         router.back();
     }
 
+    const durations = [
+        { title: "1 minute", duration: 1 * 60 },
+        { title: "5 minutes", duration: 5 * 60 },
+        { title: "10 minutes", duration: 10 * 60 },
+        { title: "15 minutes", duration: 15 * 60 },
+        { title: "30 minutes", duration: 30 * 60 },
+        { title: "1 hour", duration: 60 * 60 },
+        { title: "2 hours", duration: 120 * 60 }
+    ];
+
     return (
         <View className="flex-1 relative">
             <AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
                 <Pressable onPress={()=> router.back()} className="absolute mt-12 left-6 z-10">
                     <Ionicons name="arrow-back-circle-outline" size={48} color="white" />
                 </Pressable>
-                <View className="justify-center h-4/5">
-                    <Text className="text-center font-bold text-3xl text-white mt-32 pb-5">
+                <View className="justify-center">
+                    <Text className="text-center font-bold text-3xl text-white mt-10 pb-5">
                         Adjust your meditation duration
                     </Text>
-                    <View>
-                        <CustomButton title="1 minute" onPress={()=>(handlePress(1*60))} containerStyles="mb-5"/>
-                        <CustomButton title="5 minutes" onPress={()=>(handlePress(5*60))} containerStyles="mb-5"/>
-                        <CustomButton title="10 minutes" onPress={()=>(handlePress(10*60))} containerStyles="mb-5"/>
-                        <CustomButton title="15 minutes" onPress={()=>(handlePress(15*60))} containerStyles="mb-5"/>
-                        <CustomButton title="30 minutes" onPress={()=>(handlePress(30*60))} containerStyles="mb-5"/>
-                        <CustomButton title="1 hour" onPress={()=>(handlePress(60*60))} containerStyles="mb-5"/>
-                        <CustomButton title="2 hours" onPress={()=>(handlePress(120*60))} containerStyles="mb-1"/>
-                    </View>
+                    <ScrollView>
+                        {durations.map(({ title, duration }, index) => (
+                            <CustomButton containerStyles="mt-5" key={index} title={title} onPress={()=>handlePress(duration)} />
+                        ))}
+                    </ScrollView>
                 </View>
             </AppGradient>
         </View>
